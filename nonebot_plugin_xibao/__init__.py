93129ce5f586715b3ea610dffe73a466d4027448
insp2 <theinsp2@outlook.com>
2025-06-20 20:54:00 +0800

    优化

--- nonebot_plugin_xibao/__init__.py
+++ nonebot_plugin_xibao/__init__.py
@@ -35,7 +35,7 @@ async def _generate_image(bg_file: str, text = "", font_size = 250, text_color =
     text_height = bbox[3] - bbox[1]
     image_width, image_height = img.size
 
-    x = (image_width - text_width) / 2 + font_size / 4
+    x = (image_width - text_width) / 2
     y = (image_height - text_height) / 2 - font_size / 4
 
     draw.text((x, y), text, fill=text_color, font=font, stroke_fill=stroke, stroke_width=10)
@@ -55,9 +55,14 @@ genxibao = on_command("喜报", aliases={"喜报：", "喜报:"} )
 @genxibao.handle()
 async def xibaohandle(args:Message = CommandArg()):
     textinput = args.extract_plain_text()
-    if len(textinput) > 25:
-        await genbeibao.finish("字数太多啦！长度应在 25 个字符以内")
-    size = 250 - len(textinput) * 5
+    if len(textinput) >= 20:
+        await genbeibao.finish("字数太多啦！长度应在 20 个字符以内。")
+    elif len(textinput) < 10:
+        size = 250 - len(textinput) * 8
+    elif 15 < len(textinput) < 20:
+        size = 250 - len(textinput) * 9
+    else:
+        size = 250 - len(textinput) * 10
     picdata = await gen_xibao(text = textinput, font_size=size)
     await saa.Image(picdata).send()
 
@@ -66,8 +71,13 @@ genbeibao = on_command("悲报", aliases={"悲报：", "悲报:"} )
 @genbeibao.handle()
 async def beibaohandle(args:Message = CommandArg()):
     textinput = args.extract_plain_text()
-    if len(textinput) > 25:
-        await genbeibao.finish("字数太多啦！长度应在 25 个字符以内")
-    size = 250 - len(textinput) * 5
+    if len(textinput) >= 20:
+        await genbeibao.finish("字数太多啦！长度应在 20 个字符以内。")
+    elif len(textinput) < 10:
+        size = 250 - len(textinput) * 8
+    elif 15 < len(textinput) < 20:
+        size = 250 - len(textinput) * 9
+    else:
+        size = 250 - len(textinput) * 10
     picdata = await gen_beibao(text = textinput, font_size=size)
     await saa.Image(picdata).send()
